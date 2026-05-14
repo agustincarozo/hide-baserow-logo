96607419348b90d2247514ad3b4fc074a75282c6
agustincarozo <36301993+agustincarozo@users.noreply.github.com>
2026-05-14 09:49:00 -0300

    Update hide baserow logo.js

--- hide baserow logo.js
+++ hide baserow logo.js
@@ -1,18 +1,11 @@
-console.log('Custom Baserow script file loaded')
-
 window.__baserow.hook('app:mounted', () => {
-  console.log('Baserow app mounted')
-
   const removeLabels = () => {
-    const labels = document.querySelectorAll('.logo__label')
-    console.log('Found .logo__label elements:', labels.length)
-    labels.forEach((el) => el.remove())
+    document.querySelectorAll('.logo__label').forEach((el) => el.remove())
   }
 
   removeLabels()
 
   const observer = new MutationObserver(() => {
-    console.log('DOM changed, checking for logo labels again')
     removeLabels()
   })
 
@@ -20,6 +13,4 @@ window.__baserow.hook('app:mounted', () => {
     childList: true,
     subtree: true,
   })
-
-  console.log('MutationObserver attached')
 })
